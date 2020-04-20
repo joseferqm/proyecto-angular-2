04e1a7c87f09ab0d3f82476fd58625ecf87064ae
José Fernando Quirós Montero <jfqm26@gmail.com>
2020-04-20 11:46:56 -0600

    añade toaster a módulo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {UserService} from './shared/user.service';
 import {RouteGuard} from './shared/route-guard';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot() // ToastrModule added
+  ],
   providers: [UserService, RouteGuard],
   bootstrap: [AppComponent]
 })

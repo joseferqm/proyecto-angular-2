dd884ad20b2a19ec000bb80136f1f7fe43df3b21
José Fernando Quirós Montero <jfqm26@gmail.com>
2020-05-12 12:21:32 -0600

    agrega ruta para location

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {RouteGuard} from './shared/route-guard';
 import {AuthorComponent} from './author/author.component';
+import {LocationComponent} from './location/location.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [RouteGuard]},
   {path: 'home', component: HomeComponent, canActivate: [RouteGuard]},
   {path: 'author/:authorName', component: AuthorComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'location', component: LocationComponent}
 ];
 
 @NgModule({

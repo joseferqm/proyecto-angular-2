b39709558dfa78c4b575fd2388f86d41a5336926
José Fernando Quirós Montero <jfqm26@gmail.com>
2020-05-12 12:30:04 -0600

    pone form en location

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import { GeoService } from '../shared/geo.service';
+import {GeoService} from '../shared/geo.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-location',
@@ -10,4 +11,8 @@ export class LocationComponent implements OnInit {
   constructor(private geoService: GeoService) {}
 
   ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    console.log('test, get location');
+  }
 }

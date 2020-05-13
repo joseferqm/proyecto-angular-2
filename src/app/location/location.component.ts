87cf7c8477fb0f1bbbc62af105cab6fb82c03f01
José Fernando Quirós Montero <jfqm26@gmail.com>
2020-05-13 10:32:13 -0600

    despliegue de coords del mapa y envío de coords del form

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {LocationService} from '../shared/location.service';
 import {NgForm} from '@angular/forms';
 import {SpinnerService} from '../shared/spinner.service';
@@ -9,31 +9,63 @@ import {SpinnerService} from '../shared/spinner.service';
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent implements OnInit {
+  private display?: google.maps.LatLngLiteral;
+
   constructor(private locationService: LocationService, private spinnerService: SpinnerService) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
     const descr = form.value.descr;
-    const lon = form.value.lon;
-    const lat = form.value.lat;
-    const coords = [-16.130262, 153.605347];
+    const parsedCoords = this.parseCoords(form.value.lat, form.value.lon);
 
-    this.spinnerService.showMainSpinner();
-    this.locationService
-      .addNewLocationAsync(descr, coords)
-      .then((value) => {
-        console.log('location added!', value);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        this.spinnerService.hideMainSpinner();
-      });
+    if (parsedCoords.valid) {
+      const coords = [parsedCoords.lat, parsedCoords.lon];
+      this.spinnerService.showMainSpinner();
+      this.locationService
+        .addNewLocationAsync(descr, coords)
+        .then((value) => {
+          console.log('location added!', value);
+          form.reset();
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.spinnerService.hideMainSpinner();
+        });
+    }
+  }
+
+  parseCoords(latString: string, lonString: string) {
+    const result = {
+      lat: Number.parseFloat(latString),
+      lon: Number.parseFloat(lonString),
+      valid: null
+    };
+    result.valid = !Number.isNaN(result.lat) && !Number.isNaN(result.lon);
+    return result;
   }
 
   findLocation(form: NgForm) {
     console.log('find location');
+
+    const parsedCoords = this.parseCoords(form.value.lat, form.value.lon);
+
+    if (parsedCoords.valid) {
+      const coords = [parsedCoords.lat, parsedCoords.lon];
+      console.log('latitude', coords[0]);
+      console.log('longitude', coords[1]);
+    }
+  }
+
+  move(event: google.maps.MouseEvent) {
+    this.display = event.latLng.toJSON();
+  }
+
+  getDisplay() {
+    return this.display
+      ? {lat: this.display.lat.toFixed(4), lon: this.display.lng.toFixed(4)}
+      : {lat: 0.0, lon: 0.0};
   }
 }
